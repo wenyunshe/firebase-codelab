bb9b6dae4d673e87f9d448be221052b65777f009
Wenyun She <wehuin1215@gmail.com>
2024-11-14 17:08:06 +0800

    Show signin state

--- auth-service-worker.js
+++ auth-service-worker.js
@@ -8,11 +8,11 @@ let firebaseConfig;
 self.addEventListener('install', event => {
   // extract firebase config from query string
   const serializedFirebaseConfig = new URL(location).searchParams.get('firebaseConfig');
-  
+
   if (!serializedFirebaseConfig) {
     throw new Error('Firebase Config object not found in service worker query string.');
   }
-  
+
   firebaseConfig = JSON.parse(serializedFirebaseConfig);
   console.log("Service worker installed with Firebase config", firebaseConfig);
 });
@@ -25,10 +25,25 @@ self.addEventListener("fetch", (event) => {
 
 // TODO: add Firebase Authentication headers to request
 async function fetchWithFirebaseHeaders(request) {
-  return await fetch(request);
+  const app = initializeApp(firebaseConfig)
+  const auth = getAuth(app)
+  const installations = getInstallations(app)
+  const headers = new Headers(request.headers)
+  const [authIdToken, installationToken] = await Promise.all([
+    getAuthIdToken(auth),
+    getToken(installations)
+  ])
+  headers.append('Firebase-Instance-ID-Token', installationToken)
+  if (authIdToken) {
+    headers.append('Authorization', `Bearer ${authIdToken}`)
+  }
+  const newRequest = new Request(request, { headers })
+  return await fetch(newRequest)
 }
 
 // TODO: get user token
 async function getAuthIdToken(auth) {
-  throw new Error('not implemented');
+  await auth.authGetReady()
+  if (!auth.currentUser) return
+  return await getIdToken(auth.currentUser)
 }
